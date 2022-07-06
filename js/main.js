80c59c91bc7a8b8af7785d4d2b7442767df9a0d3
Eros D <gomezeros@gmail.com>
2022-07-06 16:04:26 -0300

    Uso de DOMs

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let track2 = {nombre: "The Adventurer", velocidad: "122 BPM", genero: "Progressi
 let track3 = {nombre: "Wandering Souls", velocidad: "124 BPM", genero: "Melodic House & Techno"}
 let track4 = {nombre: "Out of Alignment", velocidad: "124 BPM", genero: "Melodic House & Techno"}
 
-const cualidadArchivos = [WAV, AIFF, FLAC, MP3, M4A]
+const calidadArchivos = [WAV, AIFF, FLAC, MP3, M4A]
 
 let WAV = {formato: "sin pérdidas", tipo: "sin compresión", precio: 2.5}
 let AIFF = {formato: "sin pérdidas", tipo: "sin compresión", precio: 2.5}
@@ -14,9 +14,10 @@ let FLAC = {formato: "sin pérdidas", tipo: "sin compresión", precio: 2.5}
 let MP3 = {formato: "con pérdidas", tipo: "comprimido", precio: 2}
 let M4A = {formato: "con pérdidas", tipo: "comprimido", precio: 1.5}
 */
+
 elegirTrack()
 
-const tracks = [
+const tracksInfo = [
     { id: 1, artista: "Eros David", nombre: "Wandering Souls", genero: "Melodic House & Techno", anio: 2020},
     { id: 2, artista: "Eros David", nombre: "Pillars of Creation", genero: "Melodic House & Techno", anio: 2022},
     { id: 3, artista: "Eros David", nombre: "Visionaries", genero: "Organic House / Downtempo", anio: 2022},
@@ -34,20 +35,21 @@ class Formato {
     }
 }
 
-const formatos = Formato [
-    { id: 1, formato: ".WAV", tipo: "no comprimido, sin pérdidas.", precio: 2.5},
-    { id: 2, formato: ".AIFF", tipo: "no comprimido, sin pérdidas.", precio: 2.5},
-    { id: 3, formato: ".FLAC", tipo: "no comprimido, sin pérdidas.", precio: 2.5},
-    { id: 4, formato: ".MP3", tipo: "comprimido.", precio: 2},
-    { id: 5, formato: ".M4A", tipo: "comprimido", precio: 1.75}
+const formatos = [
+    { id: 1, formato: ".WAV", tipo: "no comprimido, sin pérdidas.", precio: 750},
+    { id: 2, formato: ".AIFF", tipo: "no comprimido, sin pérdidas.", precio: 750},
+    { id: 3, formato: ".FLAC", tipo: "no comprimido, sin pérdidas.", precio: 750},
+    { id: 4, formato: ".MP3", tipo: "comprimido.", precio: 500},
+    { id: 5, formato: ".M4A", tipo: "comprimido", precio: 300}
 ];
-
+/*
 for (const compra of formatos) {
     console.log(compra.id);
     console.log(compra.formato);
     console.log(compra.tipo);
     console.log(compra.precio);
 }
+*/
 
 function elegirTrack(){
     let opcionTrack = prompt('¿Qué pista desea descargar? \n Escribe 1 para descargar "Wandering Souls" de Eros David \n Escribe 2 para descargar "Pillars of Creation" de Eros David \n Escribe 3 para descargar "Visionaries" de Eros David \n Escribe 4 para descargar "The Adventurer" de Eros David \n Escribe 5 para descargar "Kalpa" de Eros David \n Escribe 6 para descargar "Out of Alignment" de Eros David \n 0 si desea cancelar la compra.')
@@ -85,8 +87,9 @@ function elegirTrack(){
 }
 elegirTrack()
 
+
 function elegirCalidad(){
-    let opcionCalidad = prompt('¿En qué calidad de audio te gustaría comprar esta canción? \n Escribe 1 para .WAV \n Escribe 2 para .AIFF \n Escribe 3 para .FLAC \n Escribe 4 para .MP3 \n Escribe 5 para .M4A \n 0 si desea cancelar la compra.')
+    let opcionCalidad = prompt('¿En qué calidad de audio te gustaría descargar esta canción? \nEscribe 1 para .WAV \nEscribe 2 para .AIFF \nEscribe 3 para .FLAC \nEscribe 4 para .MP3 \nEscribe 5 para .M4A \n0 si desea cancelar la compra.')
     if (opcionCalidad == 1 || opcionCalidad == '1') {
         alert ('Ha seleccionado la calidad en formato .WAV')
         sumarTrack()
@@ -117,6 +120,15 @@ function elegirCalidad(){
 }
 // elegirCalidad()
 
+// Carrito
+const carrito = []
+
+// Carrito completo
+function carritoCompleto(){
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0)
+}
+
+
 function sumarTrack(){
     let otraPista = prompt('¿Desea descargar otra pista? (SI/NO)')
     let respuestaOtra = otraPista.toUpperCase()
@@ -125,7 +137,7 @@ function sumarTrack(){
     }
     else if(respuestaOtra == 'NO'){
         alert(`Su carrito es de: \n ${JSON.stringify(carrito)}`)
-        const total = carritoCompleto()
+   //   const total = carritoCompleto()
         alert('¡Gracias por su compra!')
         pay()
     }
@@ -135,17 +147,10 @@ function sumarTrack(){
     }
 }
 
-// Carrito
-const carrito = []
 
-// Carrito completo
-function carritoCompleto(){
-    return carrito.reduce((acc, el) => acc + el.precio, 0)
-}
-
-// Pago
-let metodosPago = ["DEBITO", "TRANSFERENCIA", "CREDITO"]
-
+/* Pago
+let metodosPago = ["TRANSFERENCIA", "CREDITO"]
+*/
 function pay() {
     let payment = prompt("Seleccione un método de pago:\nEscriba 1 para pagar con transferencia (10% de descuento)\nEscriba 2 para pagar con tarjeta de crédito (monto variable).\n0 para ir al menú anterior.")
     if(payment == 1 || payment == '1') {
@@ -168,6 +173,83 @@ function pay() {
         pay()
     }
 }
+
+// DOMs
+const listaTracks = [
+    {
+        id: 1,
+        nombre: "Wandering Souls",
+        artista: "Eros David",
+        genero: "Melodic House & Techno",
+        anio: 2020,
+//      calidad: segun eleccion
+//      precio: como poner que sea variable segun la calidad?
+    },
+    {
+        id: 2,
+        nombre: "Pillars of Creation",
+        artista: "Eros David",
+        genero: "Melodic House & Techno",
+        anio: 2022,
+    },
+    {
+        id: 3,
+        nombre: "Visionaries",
+        artista: "Eros David",
+        genero: "Organic House / Downtempo",
+        anio: 2022,
+    },
+    {
+        id: 4,
+        nombre: "The Adventurer",
+        artista: "Eros David",
+        genero: "Progressive House",
+        anio: 2022,
+    },
+    {
+        id: 5,
+        nombre: "Kalpa",
+        artista: "Eros David",
+        genero: "Progressive House",
+        anio: 2022,
+    },
+    {
+        id: 6,
+        nombre: "Out of Alignment",
+        artista: "Eros David",
+        genero: "Melodic House & Techno",
+        anio: 2022,
+    },
+];
+
+// EXTRACCION DE DATOS DEL HTML
+let trackName = document.getElementsByClassName("track-name");
+console.log(trackName[0].innerHTML);
+console.log(trackName[1].innerHTML);
+console.log(trackName[2].innerHTML);
+console.log(trackName[3].innerHTML);
+console.log(trackName[4].innerHTML);
+console.log(trackName[5].innerHTML);
+
+let trackArtist = document.getElementsByClassName("track-artist");
+console.log(trackArtist[0].innerHTML);
+console.log(trackArtist[1].innerHTML);
+console.log(trackArtist[2].innerHTML);
+console.log(trackArtist[3].innerHTML);
+console.log(trackArtist[4].innerHTML);
+console.log(trackArtist[5].innerHTML);
+
+let trackGenre = document.getElementsByClassName("track-genre");
+console.log(trackGenre[0].innerHTML);
+console.log(trackGenre[1].innerHTML);
+console.log(trackGenre[2].innerHTML);
+console.log(trackGenre[3].innerHTML);
+console.log(trackGenre[4].innerHTML);
+console.log(trackGenre[5].innerHTML);
+
+let plantilla = `Release N°: ${tracksInfo.id} - Nombre: ${tracksInfo.nombre} - Genero: ${tracksInfo.genero} - Año: ${tracksInfo.anio}`;
+
+console.log(plantilla)
 
 
 /*
