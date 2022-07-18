68b55220bbe041d0dd6cac650e44428f5eb1602f
Eros D <gomezeros@gmail.com>
2022-07-18 18:19:55 -0300

    Segunda entrega

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,76 @@
+// Login + Registro
+
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+// Cambio de formularios
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        
+// AJAX / Fetch (login)
+
+        setFormMessage(loginForm, "Error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+
+function registrarUsuario() {
+    registros.forEach((registro) => {
+        let agregarRegistro = document.createElement();
+        agregarRegistro.innerHTML = `
+        
+        `
+
+    });
+}
+
+
+// Eventos
+
 let formulario = document.getElementById("formulario");
 let inputNombre = document.getElementById("inputName");
 let inputCorreo = document.getElementById("inputEmail");
@@ -34,12 +107,10 @@ function validarFormulario (event) {
 
     console.log(registros);
 
-    /*
     console.log(event.target);
     console.log(inputNombre.value)
     console.log(inputCorreo.value)
     console.log(inputContrasenia.value)
     console.log(inputRepetirContrasenia.value)
-    */
-    alert("Registro exitoso!")
-}
+
+    alert("Registro exitoso!")};
